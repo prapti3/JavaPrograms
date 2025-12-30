a8c542671e78c82121a8bdb632c30f52c7ae7f70
prapti3 <prapti.chavan3@gmail.com>
2025-12-30 21:57:30 +0530

    Java learning

---
+++ javabyRaj/JavaPrograms/JavaProgramsCMD/Welcome.java
@@ -0,0 +1,28 @@
+//to display welcome message onto the screen 
+
+class Welcome
+{
+   
+     public static void main(String[] args)
+     {
+        System.out.println("Welcome");
+     } 
+}
+
+class A
+{
+   public static void main(String[] args)
+   { 
+       System.out.println("A class");
+   }
+}
+
+class B
+{
+  public static void main(String[] args)
+  {
+       System.out.println("B class");
+   } 
+
+}
+
