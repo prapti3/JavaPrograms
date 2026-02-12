3f7e481ce4969d99307a4bdd69b31b35905a7027
prapti3 <prapti.chavan3@gmail.com>
2026-02-13 03:03:27 +0530

    Java Basics

---
+++ Java Self Learning Program/04CMD/JavaPrograms/Welcome.java
@@ -0,0 +1,26 @@
+//to display Welcome Message onto the screen
+
+class Welcome {
+
+     public static void main(String[] args){
+          
+        System.out.println("Welcome");
+
+
+     }
+
+}
+
+class A {
+
+
+  public static void main(String x[]){
+
+     System.out.println("A Class");
+
+}
+
+}
+
+
+class B {}
